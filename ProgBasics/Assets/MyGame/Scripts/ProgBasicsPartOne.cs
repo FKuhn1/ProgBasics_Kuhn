b925029bf2edabde0ce5ce59e5f506f5440bbb3f
Kuhn <fabian.kuhn@htl-salzburg.ac.at>
2019-03-13 17:43:29 +0100

    update scripts

--- ProgBasics/Assets/MyGame/Scripts/ProgBasicsPartOne.cs
+++ ProgBasics/Assets/MyGame/Scripts/ProgBasicsPartOne.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ProgBasicsPartOne : MonoBehaviour {
 
+    private int index = 0;
+    private int step = 5;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,39 @@ public class ProgBasicsPartOne : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        CountUp(3);
+        Text("My String Text");
+        MultNumbers(3.3f, 1.0f);
+        MixTypes("Hello", 5, 2.4f);
 	}
+
+    private void CountUp()
+    {
+        //index = index + step;
+        index += step;
+        Debug.Log(index);
+    }
+
+    private void CountUp(int myStep)
+    {
+        index = index + myStep;
+        Debug.Log(index);
+    }
+
+    private void Text(string text)
+    {
+        Debug.Log(text);
+    }
+
+    private void MultNumbers (float first, float second)
+    {
+        float tmp = first * second;
+        Debug.Log("MultNumbers: " + tmp);
+    }
+
+    private void MixTypes (string test, int index, float end)
+    {
+        Debug.Log(test + " , " + index + " , " + end);
+    }
+
 }
